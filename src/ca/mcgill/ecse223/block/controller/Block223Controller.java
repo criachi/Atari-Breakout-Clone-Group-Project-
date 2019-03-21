59ebc96a9ef8d70a6b54ae083699a89d9d3c839c
melis <melis.malki@mail.mcgill.ca>
2019-03-21 17:00:05 -0400

    hallof fame methods (exceptions included) with one error

--- src/ca/mcgill/ecse223/block/controller/Block223Controller.java
+++ src/ca/mcgill/ecse223/block/controller/Block223Controller.java
@@ -639,15 +639,68 @@ public class Block223Controller {
 	// play mode
 
 		public static List<TOPlayableGame> getPlayableGames() throws InvalidInputException {
+			return null;
 		}
 
 		public static TOCurrentlyPlayedGame getCurrentPlayableGame() throws InvalidInputException {
+			return null;
 		}
 
 		public static TOHallOfFame getHallOfFame(int start, int end) throws InvalidInputException {
+			if(!(Block223Application.getCurrentUserRole() instanceof Player)) {
+				throw new InvalidInputException("Player privileges are required to access a game's hall of fame. ");
+			}
+			if (getCurrentPlayableGame() == null) {
+				throw new InvalidInputException("A game must be selected to view its hall of fame.");
+			} 
+			List<TOPlayableGame> list = getPlayableGames ();
+			PlayedGame pgame = list.selectPlayableGame(pgame.getGame().getName(), pgame.getId());
+			Game game = pgame.getGame();
+			TOHallOfFame result = new TOHallOfFame (game.getName());
+			if (start < 1) {
+				start =1;
+			}
+			if(end > game.numberOfHallOfFameEntries()) {
+				end = game.numberOfHallOfFameEntries();
+				start = start -1;
+				end = end -1;
+			}
+			for(int i= start ; i<= end; i++) {
+				String userName = pgame.getPlayername();
+				TOHallOfFameEntry to = new TOHallOfFameEntry(i+1, userName, game.getHallOfFameEntry(i).getScore(),result);
+			}
+			return result;
 		}
 
 		public static TOHallOfFame getHallOfFameWithMostRecentEntry(int numberOfEntries) throws InvalidInputException {
+			if(!(Block223Application.getCurrentUserRole() instanceof Player)) {
+				throw new InvalidInputException("Player privileges are required to access a game's hall of fame. ");
+			}
+			if (getCurrentPlayableGame() == null) {
+				throw new InvalidInputException("A game must be selected to view its hall of fame.");
+			} 
+			List<TOPlayableGame> list = getPlayableGames ();
+			PlayedGame pgame = list.selectPlayableGame(pgame.getGame().getName(), pgame.getId());
+			Game game = pgame.getGame();
+			TOHallOfFame result = new TOHallOfFame (game.getName());
+			HallOfFameEntry mostRecent = game.getMostRecentEntry();
+			int index = game.indexOfHallOfFameEntry(mostRecent);
+			int start = index - numberOfEntries /2;
+			int end;
+			if (start < 1) {
+				start =1;
+				end = start + numberOfEntries -1;			
+			}
+			if(end > game.numberOfHallOfFameEntries()) {
+				end = game.numberOfHallOfFameEntries();
+				start = start -1;
+				end = end -1;
+			}
+			for(index =start; index<end; index++) {
+				String userName = pgame.getPlayername();
+				TOHallOfFameEntry to = new TOHallOfFameEntry(index+1, userName, game.getHallOfFameEntry(index).getScore(),result);
+			}
+			return result;
 		}
 
 
