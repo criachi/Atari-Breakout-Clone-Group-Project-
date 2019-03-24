c451574b60107b73c0305c2a4b0eabf702f3adde
criachi <christina.riachi@mail.mcgill.ca>
2019-03-24 11:45:04 -0400

    check validation checks and fix persistence

--- src/ca/mcgill/ecse223/block/model/HallOfFameEntry.java
+++ src/ca/mcgill/ecse223/block/model/HallOfFameEntry.java
@@ -5,7 +5,7 @@ package ca.mcgill.ecse223.block.model;
 import java.io.Serializable;
 
 // line 64 "../../../../../Block223PlayMode.ump"
-// line 112 "../../../../../Block223Persistence.ump"
+// line 121 "../../../../../Block223Persistence.ump"
 public class HallOfFameEntry implements Serializable
 {
 
@@ -188,7 +188,7 @@ public class HallOfFameEntry implements Serializable
   // DEVELOPER CODE - PROVIDED AS-IS
   //------------------------
   
-  // line 115 "../../../../../Block223Persistence.ump"
+  // line 124 "../../../../../Block223Persistence.ump"
   private static final long serialVersionUID = 6404159261985579849L ;
 
   
