4fa27504d0185710ee3dccd9c611ca4866ded708
criachi <christina.riachi@mail.mcgill.ca>
2019-04-07 08:20:51 -0400

    push stuff

--- src/ca/mcgill/ecse223/block/view/HallOfFameView.java
+++ src/ca/mcgill/ecse223/block/view/HallOfFameView.java
@@ -1,5 +1,27 @@
 package ca.mcgill.ecse223.block.view;
 
-public class HallOfFameView {
+import java.util.ArrayList;
 
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import ca.mcgill.ecse223.block.controller.TOHallOfFame;
+
+public class HallOfFameView extends JPanel {
+	
+	public HallOfFameView(TOHallOfFame hof) {
+		super();
+		setSize(390,390);
+		initialize(hof);
+	}
+	
+	public void initialize(TOHallOfFame hof) {
+		//ArrayList<JLabel> jLabels = new ArrayList<JLabel>(hof.numberOfEntries());
+		for(int i=0; i==hof.numberOfEntries()-1; i++) {
+			String number = String.valueOf(i+1);
+			JLabel hofEntry = new JLabel(number + hof.getEntries().get(i).getPlayername() + hof.getEntries().get(i).getScore());
+			hofEntry.setLocation(5,30+i*20);
+			add(hofEntry);
+		}
+	}
 }
